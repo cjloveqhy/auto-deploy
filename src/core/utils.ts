5971a100bce8ef81370cffcdaeca6b72c862a648
cjloveqhy <15557422216@139.com>
2025-04-08 22:20:13 +0800

    fix: update

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,6 +1,6 @@
 import * as path from "path";
 import * as fs from "node:fs";
-import * as consola from "consola";
+import { consola } from "consola";
 import chalk from "chalk";
 import {spawn} from "child_process";
 import {ConfigLayerMeta, loadConfig, ResolvedConfig, UserInputConfig} from "c12";
